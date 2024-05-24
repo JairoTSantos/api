a650fdef698bb948cd14741e1ffcf82e5ce60a46
Jairo Santos <107869809+JairoTSantos@users.noreply.github.com>
2024-05-24 11:49:52 -0300

    v1

--- src/rotas/login.route.ts
+++ src/rotas/login.route.ts
@@ -11,6 +11,117 @@ import Usuario from '../modelos/usuario.modelo';
 
 const router = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Login
+ *     description: Endpoints relacionados à autenticação de usuários
+ * /api/login:
+ *   post:
+ *     summary: Endpoint para autenticar usuário
+ *     description: Retorna um token de autenticação se as credenciais estiverem corretas
+ *     tags:
+ *       - Login
+ *     parameters:
+ *       - in: body
+ *         name: body
+ *         required: true
+ *         description: Objeto JSON contendo o email e senha do usuário
+ *         schema:
+ *           type: object
+ *           properties:
+ *             email:
+ *               type: string
+ *               example: email@email.com
+ *             senha:
+ *               type: string
+ *               example: senha
+ *     responses:
+ *       200:
+ *         description: Login bem-sucedido
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: number
+ *                   example: 200
+ *                 message:
+ *                   type: string
+ *                   example: Login feito com sucesso
+ *                 token:
+ *                   type: string
+ *                   example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3VhcmlvX25vbWUiOiJKYWlybyBKZWZmZXJzb25lIFRleGVpcmEiLCJ1c3VhcmlvX25pdmVsIjoxLCJ1c3VhcmlvX2VtYWlsIjoiZW1haWxAZXhhbXBsZS5jb20iLCJ1c3VhcmlvX2lkIjoxfQ.k_rH2fYI9Hv74cugSewicXCi_CXC0oH4wF3ugHoP4Is
+ *       400:
+ *         description: Campos obrigatórios não preenchidos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: number
+ *                   example: 400
+ *                 message:
+ *                   type: string
+ *                   example: Preencha os campos obrigatórios
+ *       401:
+ *         description: Senha incorreta
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: number
+ *                   example: 401
+ *                 message:
+ *                   type: string
+ *                   example: Senha incorreta
+ *       403:
+ *         description: Usuário desativado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: number
+ *                   example: 403
+ *                 message:
+ *                   type: string
+ *                   example: Usuário desativado
+ *       404:
+ *         description: Email incorreto
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: number
+ *                   example: 404
+ *                 message:
+ *                   type: string
+ *                   example: Email incorreto
+ *       500:
+ *         description: Erro interno do servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: number
+ *                   example: 500
+ *                 message:
+ *                   type: string
+ *                   example: Erro interno do servidor
+ *                 error:
+ *                   type: string
+ */
+
 
 router.post('/login', async (req: Request, res: Response) => {
     const email = req.body.email;
--- src/swaggerConfig.ts
+++ src/swaggerConfig.ts
@@ -1,15 +1,15 @@
 import swaggerJsdoc from 'swagger-jsdoc';
 
 const options: swaggerJsdoc.Options = {
-  swaggerDefinition: {
-    info: {
-      title: 'Gabinete Digital',
-      version: '1.0.0',
-      description: 'Backend aplicação Gabinete Digital',
+    swaggerDefinition: {
+        info: {
+            title: 'Gabinete Digital',
+            version: '1.0.0',
+            description: 'Backend aplicação Gabinete Digital',
+        },
+        servers: [{ url: 'http://localhost:3000/api' }],
     },
-    servers: [{ url: 'http://localhost:3000' }],
-  },
-  apis: ['src/rotas/*.ts'], 
+    apis: ['src/rotas/*.ts'],
 };
 
 const specs = swaggerJsdoc(options);
